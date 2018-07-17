bda66781491ba024de4e55e0c43c3906636c3a1d
ATran <tranxuanai9x@gmail.com>
2018-07-17 10:13:23 +0700

    Nâng cấp phiên bản RN 0.56

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
-const { start } = require('./App');
+// const { start } = require('./App');
 
-start();
+// start();
 
+/** @format */
+
+import {AppRegistry} from 'react-native';
+import App from './App';
+import {name as appName} from './app.json';
+
+AppRegistry.registerComponent(appName, () => App);
